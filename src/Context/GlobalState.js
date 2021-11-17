76307d019c65fc2a967bfd36250684bd4e17ac1f
Ahmed Amir Salem (Kenway The Great) <ahmad_amir@live.com>
2021-11-17 08:25:27 +0200

    Added Appreducer file and added context provider

--- src/Context/GlobalState.js
+++ src/Context/GlobalState.js
@@ -1,7 +1,22 @@
 import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
 
 const initialState = {
-  transactions: [],
+  transactions: [
+    { id: 1, text: "Flower", amount: -20 },
+    { id: 1, text: "Salary", amount: 300 },
+    { id: 1, text: "Book", amount: -10 },
+    { id: 1, text: "Camera", amount: 150 },
+  ],
 };
 
 export const GlobalContext = createContext(initialState);
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+  return (
+    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
